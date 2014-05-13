a77ee64094b8743123ee6c4cddb31c2b09288b7c
jwalnes <jwalnes@drw.com>
2014-05-13 14:21:11 -0500

    Main library

---
+++ tinytest.js
@@ -0,0 +1,89 @@
+/**
+ * Very simple in-browser unit-test library, with zero deps.
+ *
+ * Background turns green if all tests pass, otherwise red.
+ * View the JavaScript console to see failure reasons.
+ *
+ * Example:
+ *
+ *   adder.js (code under test)
+ *
+ *     function add(a, b) {
+ *       return a + b;
+ *     }
+ *
+ *   adder-test.html (tests - just open a browser to see results)
+ *
+ *     <script src="tinytest.js"></script>
+ *     <script src="adder.js"></script>
+ *     <script>
+ *
+ *     tests({
+ *
+ *       'adds numbers': function() {
+ *         assertEquals(6, add(2, 4));
+ *         assertEquals(6.6, add(2.6, 4));
+ *       },
+ *
+ *       'subtracts numbers': function() {
+ *         assertEquals(-2, add(2, -4));
+ *       },
+ *
+ *     });
+ *     </script>
+ *
+ * That's it. Stop using over complicated frameworks that get in your way.
+ *
+ * -Joe Walnes
+ */
+var TinyTest = {
+
+    run: function(tests) {
+        var failures = 0;
+        for (var testName in tests) {
+            var testAction = tests[testName];
+            try {
+                testAction.apply(this);
+                console.log('Test:', testName, 'OK');
+            } catch (e) {
+                failures++;
+                console.error('Test:', testName, 'FAILED');
+                console.error(e.stack);
+            }
+        }
+        setTimeout(function() { // Give document a chance to complete
+            if (window.document && document.body) {
+                document.body.style.backgroundColor = (failures == 0 ? '#99ff99' : '#ff9999');
+            }
+        }, 0);
+    },
+
+    fail: function(msg) {
+        throw new Error('fail(): ' + msg);
+    },
+
+    assert: function(value, msg) {
+        if (!value) {
+            throw new Error('assert(): ' + msg);
+        }
+    },
+
+    assertEquals: function(expected, actual) {
+        if (expected != actual) {
+            throw new Error('assertEquals() "' + expected + '" != "' + actual + '"');
+        }
+    },
+
+    assertStrictEquals: function(expected, actual) {
+        if (expected !== actual) {
+            throw new Error('assertStrictEquals() "' + expected + '" !== "' + actual + '"');
+        }
+    },
+
+};
+
+var fail               = TinyTest.fail.bind(TinyTest);
+var assert             = TinyTest.assert.bind(TinyTest);
+var assertEquals       = TinyTest.assertEquals.bind(TinyTest);
+var assertStrictEquals = TinyTest.assertStrictEquals.bind(TinyTest);
+var tests              = TinyTest.run.bind(TinyTest);
