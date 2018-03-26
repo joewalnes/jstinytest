eef2de94b1901ba6ecb49ee636cb12d785fe8e29
Rick Hallett <rickhallett@live.co.uk>
2018-03-26 03:06:32 +0100

    Update tinytest.js
    removed function parentheses spacing

--- tinytest.js
+++ tinytest.js
@@ -39,7 +39,7 @@
  */
 const TinyTest = {
 
-    run: function (tests) {
+    run: function(tests) {
         let failures = 0;
         for (let testName in tests) {
             let testAction = tests[testName];
@@ -52,30 +52,30 @@ const TinyTest = {
                 console.error(e.stack);
             }
         }
-        setTimeout(function () { // Give document a chance to complete
+        setTimeout(function() { // Give document a chance to complete
             if (window.document && document.body) {
                 document.body.style.backgroundColor = (failures == 0 ? '#99ff99' : '#ff9999');
             }
         }, 0);
     },
 
-    fail: function (msg) {
+    fail: function(msg) {
         throw new Error('fail(): ' + msg);
     },
 
-    assert: function (value, msg) {
+    assert: function(value, msg) {
         if (!value) {
             throw new Error('assert(): ' + msg);
         }
     },
 
-    assertEquals: function (expected, actual) {
+    assertEquals: function(expected, actual) {
         if (expected != actual) {
             throw new Error('assertEquals() "' + expected + '" != "' + actual + '"');
         }
     },
 
-    assertStrictEquals: function (expected, actual) {
+    assertStrictEquals: function(expected, actual) {
         if (expected !== actual) {
             throw new Error('assertStrictEquals() "' + expected + '" !== "' + actual + '"');
         }
@@ -83,9 +83,9 @@ const TinyTest = {
 
 };
 
-const fail = TinyTest.fail,
-    assert = TinyTest.assert,
-    assertEquals = TinyTest.assertEquals,
-    eq = TinyTest.assertEquals, // alias for assertEquals
-    assertStrictEquals = TinyTest.assertStrictEquals,
-    tests = TinyTest.run;
+const fail                = TinyTest.fail,
+      assert              = TinyTest.assert,
+      assertEquals        = TinyTest.assertEquals,
+      eq                  = TinyTest.assertEquals, // alias for assertEquals
+      assertStrictEquals  = TinyTest.assertStrictEquals,
+      tests               = TinyTest.run;
