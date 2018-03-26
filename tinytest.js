bca22192ba739e6386e66be9dd2cf2eb6b74816f
whitefire0 <rickhallett@live.co.uk>
2018-03-26 02:59:52 +0100

    removed legacy bind code, updated to ES6 variable types

--- tinytest.js
+++ tinytest.js
@@ -37,14 +37,14 @@
  * -Joe Walnes
  * MIT License. See https://github.com/joewalnes/jstinytest/
  */
-var TinyTest = {
+const TinyTest = {
 
-    run: function(tests) {
-        var failures = 0;
-        for (var testName in tests) {
-            var testAction = tests[testName];
+    run: function (tests) {
+        let failures = 0;
+        for (let testName in tests) {
+            let testAction = tests[testName];
             try {
-                testAction.apply(this);
+                testAction();
                 console.log('Test:', testName, 'OK');
             } catch (e) {
                 failures++;
@@ -52,30 +52,30 @@ var TinyTest = {
                 console.error(e.stack);
             }
         }
-        setTimeout(function() { // Give document a chance to complete
+        setTimeout(function () { // Give document a chance to complete
             if (window.document && document.body) {
                 document.body.style.backgroundColor = (failures == 0 ? '#99ff99' : '#ff9999');
             }
         }, 0);
     },
 
-    fail: function(msg) {
+    fail: function (msg) {
         throw new Error('fail(): ' + msg);
     },
 
-    assert: function(value, msg) {
+    assert: function (value, msg) {
         if (!value) {
             throw new Error('assert(): ' + msg);
         }
     },
 
-    assertEquals: function(expected, actual) {
+    assertEquals: function (expected, actual) {
         if (expected != actual) {
             throw new Error('assertEquals() "' + expected + '" != "' + actual + '"');
         }
     },
 
-    assertStrictEquals: function(expected, actual) {
+    assertStrictEquals: function (expected, actual) {
         if (expected !== actual) {
             throw new Error('assertStrictEquals() "' + expected + '" !== "' + actual + '"');
         }
@@ -83,9 +83,9 @@ var TinyTest = {
 
 };
 
-var fail               = TinyTest.fail.bind(TinyTest),
-    assert             = TinyTest.assert.bind(TinyTest),
-    assertEquals       = TinyTest.assertEquals.bind(TinyTest),
-    eq                 = TinyTest.assertEquals.bind(TinyTest), // alias for assertEquals
-    assertStrictEquals = TinyTest.assertStrictEquals.bind(TinyTest),
-    tests              = TinyTest.run.bind(TinyTest);
+const fail = TinyTest.fail,
+    assert = TinyTest.assert,
+    assertEquals = TinyTest.assertEquals,
+    eq = TinyTest.assertEquals, // alias for assertEquals
+    assertStrictEquals = TinyTest.assertStrictEquals,
+    tests = TinyTest.run;
